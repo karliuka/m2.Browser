7b088355607fd75c14084bee96f5396ee8364a16
karliuka <wit.kharkov@gmail.com>
2016-12-20 21:20:17 +0200

    ui grid

---
+++ Model/ResourceModel/Info/Grid/Collection.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Faonni\Browser\Model\ResourceModel\Info\Grid;
+
+use Magento\Customer\Ui\Component\DataProvider\Document;
+use Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult;
+use Magento\Framework\Data\Collection\Db\FetchStrategyInterface as FetchStrategy;
+use Magento\Framework\Data\Collection\EntityFactoryInterface as EntityFactory;
+use Magento\Framework\Event\ManagerInterface as EventManager;
+use Psr\Log\LoggerInterface as Logger;
+
+class Collection extends SearchResult
+{
+    /**
+     * @inheritdoc
+     */
+    protected $document = Document::class;
+
+    /**
+     * Initialize dependencies
+     *
+     * @param EntityFactory $entityFactory
+     * @param Logger $logger
+     * @param FetchStrategy $fetchStrategy
+     * @param EventManager $eventManager
+     * @param string $mainTable
+     * @param string $resourceModel
+     */
+    public function __construct(
+        EntityFactory $entityFactory,
+        Logger $logger,
+        FetchStrategy $fetchStrategy,
+        EventManager $eventManager,
+        $mainTable = 'faonni_browser_info',
+        $resourceModel = '\Faonni\Browser\Model\ResourceModel\Info'
+    ) {
+        parent::__construct($entityFactory, $logger, $fetchStrategy, $eventManager, $mainTable, $resourceModel);
+    }
+}
